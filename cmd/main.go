dc61ad343e29f8780a9df455fbfc93955d5f9d3c
ralif1330 <ilmudunia75@gmail.com>
2023-08-25 19:43:33 +0700

    maintenance

--- cmd/main.go
+++ cmd/main.go
@@ -7,6 +7,7 @@ import (
 	"github.com/labstack/echo/v4"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+
 )
 
 const (
@@ -39,53 +40,7 @@ func main() {
 
 }
 
-func seedDB() {
 
-	foodMenu := []MenuItem{
-		{
-			Name:      "Tempe",
-			OrderCode: "tempe",
-			Price:     5000,
-			Type:      MenuTypeFood,
-		},
-		{
-			Name:      "Terong",
-			OrderCode: "terong",
-			Price:     7000,
-			Type:      MenuTypeFood,
-		},
-	}
-
-	drinkMenu := []MenuItem{
-		{
-			Name:      "Milo",
-			OrderCode: "milo",
-			Price:     3000,
-			Type:      MenuTypeDrink,
-		},
-		{
-			Name:      "Susu",
-			OrderCode: "susu",
-			Price:     2000,
-			Type:      MenuTypeDrink,
-		},
-	}
-
-	fmt.Println(drinkMenu, foodMenu)
-
-	db, err := gorm.Open(postgres.Open(dbAddress))
-	if err != nil {
-		panic(err)
-	}
-
-	db.AutoMigrate(&MenuItem{})
-
-	if err := db.First(&MenuItem{}).Error; err == gorm.ErrRecordNotFound {
-		db.Create(&foodMenu)
-		db.Create(&drinkMenu)
-	}
-
-}
 
 func getMenu(c echo.Context) error {
 	menuType := c.FormValue("menu_type")
